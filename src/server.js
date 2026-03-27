c2a7bee036553e34b9ad0c4f98d26d3c471fddd8
fatimah7643 <fatimah013@gmail.com>
2026-03-27 09:55:08 +0800

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
-import express from 'express';
+import express from 'express';import routes from './routes.js';
 import cors from 'cors';
-import routes from './routes.js';
-
+ 
 const app = express();
 const port = process.env.PORT || 3000;
 const host = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0';
-
-app.use(cors({ origin: '*' })); 
-app.use(express.json());       
-
+ 
+app.use(express.json());
+app.use(cors({
+  origin: '*'
+}));
 app.use('/', routes);
-
+ 
 app.listen(port, () => {
-  console.log(`Server berjalan pada http://${host}:${port}`);
+  console.log(`Server running at http://${host}:${port}`);
 });
